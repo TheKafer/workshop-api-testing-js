f351a17ac2666142c7bbedbc449d6ee5b2f0a38f
TheKafer <38733280+TheKafer@users.noreply.github.com>
2020-12-17 11:32:23 -0500

    Add Eslint (#5)
    * Add Eslint
    
    * EOF and devDependencies
    
    * correction space

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,79 +2,76 @@ const agent = require('superagent');
 const statusCode = require('http-status-codes');
 const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
-const url_base = 'https://httpbin.org/'
+const url = 'https://httpbin.org/';
 
 describe('First Api Tests', () => {
+  it('Consume GET Service', async () => {
+    const response = await agent.get(`${url}ip`);
 
-    it('Consume GET Service', async () => {
-        const response = await agent.get(url_base+'ip');
-      
-        expect(response.status).to.equal(statusCode.StatusCodes.OK);
-        expect(response.body).to.have.property('origin');
-      });
+    expect(response.status).to.equal(statusCode.StatusCodes.OK);
+    expect(response.body).to.have.property('origin');
+  });
 
-    it('Consume GET Service with query parameters', async () => {
-        const query = {
-          name: 'John',
-          age: '31',
-          city: 'New York'
-        };
-      
-        const response = await agent.get(url_base+'get').query(query);
-      
-        expect(response.status).to.equal(statusCode.StatusCodes.OK);
-        expect(response.body.args).to.eql(query);
-    });
+  it('Consume GET Service with query parameters', async () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-    it('Consume HEAD Service', async () => {
-      const response = await agent.head(url_base+'headers');
-    
-      expect(response.status).to.equal(statusCode.StatusCodes.OK);
-      expect(response.headers).to.have.property('date');
-      expect(response.headers).to.have.property('content-length');
-      expect(response.body).to.eql({});
-     
-    });
+    const response = await agent.get(`${url}get`).query(query);
 
-    it('Consume PATCH Service', async () => {
-      const args ={
-        name: 'John',
-        age: '31',
-        city: 'New York'
-      };
+    expect(response.status).to.equal(statusCode.StatusCodes.OK);
+    expect(response.body.args).to.eql(query);
+  });
 
-      const response = await agent.patch(url_base+'patch').send(args);
+  it('Consume HEAD Service', async () => {
+    const response = await agent.head(`${url}headers`);
 
-      expect(response.status).to.equal(statusCode.StatusCodes.OK);
-      expect(response.body.json).to.eql(args);
-     
-    });
+    expect(response.status).to.equal(statusCode.StatusCodes.OK);
+    expect(response.headers).to.have.property('date');
+    expect(response.headers).to.have.property('content-length');
+    expect(response.body).to.eql({});
+  });
 
-    it('Consume PUT Service', async () => {
-      const args = {
-        name: 'John',
-        age: 31,
-        city: 'New York'
-      };
-  
-      const response = await agent.put(url_base+'put').send(args);
+  it('Consume PATCH Service', async () => {
+    const args = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
 
-      expect(response.status).to.equal(statusCode.StatusCodes.OK);
-      expect(response.body.json).to.eql(args);
-    });
+    const response = await agent.patch(`${url}patch`).send(args);
 
-    it('Consume DELETE Service', async () => {
-      const args = {
-        name: 'John',
-        age: 31,
-        city: 'New York'
-      };
-  
-      const response = await agent.del(url_base+'delete').send(args);
-      
-      expect(response.status).to.equal(statusCode.StatusCodes.OK);
-      expect(response.body.json).to.eql(args);
-    });
+    expect(response.status).to.equal(statusCode.StatusCodes.OK);
+    expect(response.body.json).to.eql(args);
+  });
+
+  it('Consume PUT Service', async () => {
+    const args = {
+      name: 'John',
+      age: 31,
+      city: 'New York'
+    };
+
+    const response = await agent.put(`${url}put`).send(args);
+
+    expect(response.status).to.equal(statusCode.StatusCodes.OK);
+    expect(response.body.json).to.eql(args);
+  });
+
+  it('Consume DELETE Service', async () => {
+    const args = {
+      name: 'John',
+      age: 31,
+      city: 'New York'
+    };
+
+    const response = await agent.del(`${url}delete`).send(args);
+
+    expect(response.status).to.equal(statusCode.StatusCodes.OK);
+    expect(response.body.json).to.eql(args);
+  });
 });
